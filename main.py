ebbb498f938010099d87a00be65c890a3a83c078
hiro-v1 <mamanteng13@gmail.com>
2024-11-14 17:43:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from pytgcalls import idle
-from driver.hiroMV import call_py, bot
+from driver.eliza import call_py, bot
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
